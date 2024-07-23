7424aff2b52e22c18d7929bb5af85ca3d90da7ac
aa <aa@bb.com>
2024-07-23 08:25:12 +0800

    update zkvrf address
    Signed-off-by: Glen <glen@zkcross.org>

--- src/data/constants.js
+++ src/data/constants.js
@@ -6,7 +6,7 @@ export const WAITFOR_CONFIRMATIONS = 2;
 //export const COINFLIP_ADDRESS = "0x6d9AeE60C87f97BF3Cdce3d59C6c6C2e80F301ca"; // VRF v1 on Kovan
 // export const COINFLIP_ADDRESS = "0x5611547ae21E7d4d0235f4fFA5b4175e63343aA4"; // VRF v2 on Rinkeby
 // export const COINFLIP_ADDRESS = "0x2a492922358E2E272bdC6B9bE29e4AAfe1f1f210"; // VRF v2 on Goerli
-export const COINFLIP_ADDRESS = "0xd1b905751DC78FE6D5beC62558fDCeac9Ee1C45f"; // use zkcvrf on Sepolia
+export const COINFLIP_ADDRESS = "0x35C6501844c56FC0534447fF32FBD5fa6D488726"; // use zkcvrf on Sepolia
 
 export const COINFLIP = {
   address: COINFLIP_ADDRESS,
